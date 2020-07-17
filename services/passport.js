370db822d12a4f981b9a5f014610e4ec70a0bf4e
Dennis <beiteldennis123@gmail.com>
2020-07-17 19:57:35 +0200

    progress

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accesstoken', accessToken);
-      console.log('refreshtoken', refreshToken);
-      console.log('profile:', profile);
-      console.log('done', done);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //we already have a record with given ID
+          done(null, existingUser);
+        } else {
+          //we dont have a user record with this ID. make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
