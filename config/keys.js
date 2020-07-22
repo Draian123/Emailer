5e3a79368e43e70c0e655a6d1e517d9f874a6b5a
Dennis <beiteldennis123@gmail.com>
2020-07-22 17:08:52 +0200

    finished auth flow 5

--- config/keys.js
+++ config/keys.js
@@ -3,3 +3,11 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./dev');
 }
+
+// 922800946154-i23nr88tuldladsdgg8oak4g52gm11rp.apps.googleusercontent.com
+
+// I3id_rC5wAu2eJN8NI9Vtb8H
+
+// mongodb+srv://Draian123:kAg9HUesPSH3hQg7@cluster0.tcurp.mongodb.net/emailer?retryWrites=true&w=majority
+
+// dshfhdfhinsjntdsetsgcvjadasfhdsgfsdghj
