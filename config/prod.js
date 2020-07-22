95b090e654b7eaf784ae544a29b2958757d0e24c
Dennis <beiteldennis123@gmail.com>
2020-07-22 16:37:25 +0200

    finished auth flow 3

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,4 @@
 // production keys here
-// module.exports = {
-//   googleClientID:
-//     '922800946154-i23nr88tuldladsdgg8oak4g52gm11rp.apps.googleusercontent.com',
-//   googleClientSecret: 'I3id_rC5wAu2eJN8NI9Vtb8H',
-//   mongoURI:
-//     'mongodb+srv://Draian123:kAg9HUesPSH3hQg7@cluster0.tcurp.mongodb.net/emailer?retryWrites=true&w=majority',
-//   cookieKey: 'dshfhdfhinsjntdsetsgcvjadasfhdsgfsdghj',
-// };
 
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
