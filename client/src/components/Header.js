5520fbdfc44e3d214805c61491a31aec7b6421b4
Dennis <beiteldennis123@gmail.com>
2020-07-25 02:34:13 +0200

    Added billing and cliet side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -14,11 +15,17 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
-          </li>
-        );
+          </li>,
+        ];
     }
   }
 
@@ -29,6 +36,7 @@ class Header extends Component {
           <Link
             to={this.props.auth ? '/survey' : '/'}
             className="left brand-logo"
+            style={{ margin: '0 10px' }}
           >
             Emailer
           </Link>
