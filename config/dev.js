17a601c50bcc87572afbda2273d3d49be508ae03
Dennis <beiteldennis123@gmail.com>
2020-07-27 03:10:57 +0200

    new stuff

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+//dev keys
+module.exports = {
+  googleClientID:
+    '452529761049-247ftta07p5l5h3ncsfrpgm31k55kj4h.apps.googleusercontent.com',
+  googleClientSecret: 'ueJpQ7_va-_QHBDLFDQ2m8id',
+  mongoURI:
+    'mongodb+srv://Draian123:OJGUDLl0c5Ar6q6h@emailer.ze5g4.mongodb.net/emailer?retryWrites=true&w=majority',
+  cookieKey: 'dshfhdfhinsjntdsetsgcvjadasfhj',
+  stripePublishableKey:
+    'pk_test_51H8TGTCsQg4qrgHqslsy7qXlJBCyslLUoLOVcTspD92bD6UFGD7BbleBvIiEMDgQwfRUj3DQIRWfHoEtkhJObMwo009a6XieZ8',
+  stripeSecretKey:
+    'sk_test_51H8TGTCsQg4qrgHq0fDn2e6ggCoUhN5ANrKgPVk1BoZObAvMIj1ftLvcjzhAub8FbQDzLsRxhlZVi3fFjjJnwJnh00e0eHhzok',
+  sendGridKey:
+    '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',
+};
